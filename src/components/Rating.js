fd10ffb9c7cc955771ab5a629f0a638cf386f962
vertig0o <atilay_yoruk@outlook.com>
2024-02-25 14:27:00 +0300

    Update

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import "./Rating.css";
 import starIcon from "../images/icon-star.svg";
-import { useState } from "react";
 
 const Rating = ({ rating, setRating, setShowThankYouPage }) => {
-  const [activeRatings, setActiveRatings] = useState({
-    oneStar: false,
-    twoStar: false,
-    threeStar: false,
-    fourStar: false,
-    fiveStar: false,
-  });
+  const [activeRating, setActiveRating] = useState(0);
+
+  const handleRatingClick = (value) => {
+    setActiveRating(value);
+    setRating(value);
+  };
+
   const handleSubmit = () => {
     if (!rating) return;
     setShowThankYouPage(true);
@@ -28,106 +27,26 @@ const Rating = ({ rating, setRating, setShowThankYouPage }) => {
         appreciated to help us improve our offering!
       </p>
       <div className="rating">
-        <div
-          className={
-            activeRatings.oneStar
-              ? "rating_container active"
-              : "rating_container"
-          }
-          onClick={() => {
-            setActiveRatings({
-              oneStar: true,
-              twoStar: false,
-              threeStar: false,
-              fourStar: false,
-              fiveStar: false,
-            });
-            setRating(1);
-          }}
-        >
-          1
-        </div>
-        <div
-          className={
-            activeRatings.twoStar
-              ? "rating_container active"
-              : "rating_container"
-          }
-          onClick={() => {
-            setActiveRatings({
-              oneStar: false,
-              twoStar: true,
-              threeStar: false,
-              fourStar: false,
-              fiveStar: false,
-            });
-            setRating(2);
-          }}
-        >
-          2
-        </div>
-        <div
-          className={
-            activeRatings.threeStar
-              ? "rating_container active"
-              : "rating_container"
-          }
-          onClick={() => {
-            setActiveRatings({
-              oneStar: false,
-              twoStar: false,
-              threeStar: true,
-              fourStar: false,
-              fiveStar: false,
-            });
-            setRating(3);
-          }}
-        >
-          3
-        </div>
-        <div
-          className={
-            activeRatings.fourStar
-              ? "rating_container active"
-              : "rating_container"
-          }
-          onClick={() => {
-            setActiveRatings({
-              oneStar: false,
-              twoStar: false,
-              threeStar: false,
-              fourStar: true,
-              fiveStar: false,
-            });
-            setRating(4);
-          }}
-        >
-          4
-        </div>
-        <div
-          className={
-            activeRatings.fiveStar
-              ? "rating_container active"
-              : "rating_container"
-          }
-          onClick={() => {
-            setActiveRatings({
-              oneStar: false,
-              twoStar: false,
-              threeStar: false,
-              fourStar: false,
-              fiveStar: true,
-            });
-            setRating(5);
-          }}
-        >
-          5
-        </div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <div
+            key={value}
+            className={`rating_container ${
+              activeRating === value ? "active" : ""
+            }`}
+            onClick={() => handleRatingClick(value)}
+          >
+            {value}
+          </div>
+        ))}
       </div>
-      <button className="submit_btn" onClick={handleSubmit}>
+      <button
+        className={`submit_btn ${activeRating > 0 ? "active" : ""}`}
+        onClick={handleSubmit}
+      >
         Submit
       </button>
     </Card>
   );
 };
+
 export default Rating;
